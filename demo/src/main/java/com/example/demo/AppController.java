ae301c31f435f220ff86ecacbd9609f8ffc7bfde
zeeshan ahmad <zeeshan.ahmad@fplabs.tech>
2025-12-02 01:07:00 +0530

    first serve

---
+++ demo/src/main/java/com/example/demo/AppController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app")
+public class AppController {
+
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam String name){
+        return "Helloo    "+name +Thread.currentThread().getName();
+    }
+}
